02644a44699a8560bbe808bb1b6bfcc96ddb505d
Jorge Robles <robles8925@gmail.com>
2022-03-21 20:22:17 -0400

    part 1, small edits

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+const country = 'United States';
+const continent = 'North America';
+let population = 339500000;
+let isIsland = 'United States';
+let language;
+console.log(country, continent, population, isIsland)
+// Data Types
+
+language = 'English';
